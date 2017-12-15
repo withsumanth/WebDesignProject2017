0621754a2ff99ed6120ba5c99f4542369854126b
withsumanth <hagalavadigopalakr.s@husky.neu.edu>
2017-12-14 21:31:48 -0500

    Updated index page

--- web-design-app/src/app/index/index.component.ts
+++ web-design-app/src/app/index/index.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user/user-service';
 import {Router, NavigationExtras} from '@angular/router';
+import { ModalModule } from 'ngx-bootstrap';
+import {NgbModal, NgbActiveModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { Overlay, overlayConfigFactory } from 'angular2-modal';
 import * as $ from 'jquery';
 
 @Component({
@@ -12,7 +15,8 @@ export class IndexComponent implements OnInit {
   searchList = ["Food","Shopping","Events","Nightlife","Places of Interest","E-Learning"];
   public isUserLoggedIn = false;
   values: any[];
-  constructor(private userService: UserService , private router: Router) { }
+  closeResult: string;
+  constructor(private userService: UserService , private router: Router, public modalService: NgbModal) { }
   ngOnInit() {
     this.isUserLoggedIn = this.userService
       .isLoggedIn();
@@ -51,33 +55,47 @@ export class IndexComponent implements OnInit {
       return false;
     }
   }
-    searchValidation(){
+    searchValidation(content){
         let check: boolean = false;
-        for(var i=0;i<this.searchList.length;i++){
-            if($('#searchInput').val().toString().toUpperCase()=== this.searchList[i].toUpperCase()){
-              console.log($('#selectValue').val());
-                check = true;
-                break;
-            }
-        }
-        if(check){
-          if($('#searchInput').val().toString().toUpperCase() === 'FOOD'){
-            let navigationExtras: NavigationExtras = {
-              queryParams: {
-                "cityName": $('#selectValue').val().toString().toUpperCase()
-              }
-            };
-            this.router.navigate(['/restaurants'], navigationExtras);
-          }
-          /*this.values.add($('#selectValue').val().toString().toUpperCase());
-
-          this.userService
-            .selectData(this.values)
-            .subscribe(restaurants => {
-              this.restaurants = restaurants;}, this.nextPage());*/
+        if(!this.isUserLoggedIn){
+          this.modalService.open(content);
         }else{
-          $('#resultError').html('No Result');
-          return false;
+          for(var i=0;i<this.searchList.length;i++){
+            if($('#searchInput').val().toString().toUpperCase()=== this.searchList[i].toUpperCase()){
+              check = true;
+              break;
+            }
+          }
+          if(check){
+            if($('#searchInput').val().toString().toUpperCase() === 'FOOD'){
+              let navigationExtras: NavigationExtras = {
+                queryParams: {
+                  "cityName": $('#selectValue').val().toString().toUpperCase()
+                }
+              };
+              this.router.navigate(['/restaurants'], navigationExtras);
+            }
+            /*this.values.add($('#selectValue').val().toString().toUpperCase());
+
+            this.userService
+              .selectData(this.values)
+              .subscribe(restaurants => {
+                this.restaurants = restaurants;}, this.nextPage());*/
+          }else{
+            $('#resultError').html('No Result');
+            return false;
+          }
         }
+
     }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
